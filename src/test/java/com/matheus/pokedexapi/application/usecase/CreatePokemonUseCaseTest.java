c34e03ddca693af5aa9f700baac0bbaa9f489756
Matheus <bandeira.freitas2013@gmail.com>
2026-08-03 17:51:19 -0300

    Adicionando teste unitário.

--- src/test/java/com/matheus/pokedexapi/application/usecase/CreatePokemonUseCaseTest.java
+++ src/test/java/com/matheus/pokedexapi/application/usecase/CreatePokemonUseCaseTest.java
@@ -2,6 +2,7 @@ package com.matheus.pokedexapi.application.usecase;
 
 import com.matheus.pokedexapi.application.dto.CreatePokemonRequest;
 import com.matheus.pokedexapi.domain.entity.Pokemon;
+import com.matheus.pokedexapi.domain.exception.PokemonAlreadyExistsException;
 import com.matheus.pokedexapi.domain.repository.PokemonRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,9 +13,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CreatePokemonUseCaseTest {
@@ -57,6 +58,30 @@ public class CreatePokemonUseCaseTest {
         assertEquals("Pikachu", result.getName());
 
         verify(pokemonRepository).save(any(Pokemon.class));
+
+    }
+
+    @Test
+    void shouldThrowExceptionWhenPokemonAlreadyExists() {
+
+        CreatePokemonRequest request = new CreatePokemonRequest(
+                "Pikachu",
+                "Electric",
+                40,
+                60,
+                "Mouse Pokémon",
+                "url"
+        );
+
+        when(pokemonRepository.existsByName(anyString())).thenReturn(true);
+
+        PokemonAlreadyExistsException exception = assertThrows(PokemonAlreadyExistsException.class,() -> createPokemonUseCase.execute(request));
+
+        assertEquals("Pokemon already exists: Pikachu",exception.getMessage());
+
+        verify(pokemonRepository).existsByName("Pikachu");
+
+        verify(pokemonRepository, never()).save(any(Pokemon.class));
 
     }
 }
