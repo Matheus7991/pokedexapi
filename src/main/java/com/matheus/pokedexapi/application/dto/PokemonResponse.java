480be091c39be84f52e771f63cc7c6e3b11b727e
Matheus <bandeira.freitas2013@gmail.com>
2026-08-11 20:10:08 -0300

    Refinamento DTOs.

--- src/main/java/com/matheus/pokedexapi/application/dto/PokemonResponse.java
+++ src/main/java/com/matheus/pokedexapi/application/dto/PokemonResponse.java
@@ -1,14 +1,30 @@
 package com.matheus.pokedexapi.application.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.UUID;
 
 public record PokemonResponse(
+
+        @Schema(description = "Identificador único do Pokémon", example = "550e8400-e29b-41d4-a716-446655440000")
         UUID id,
+
+        @Schema(description = "Nome do Pokémon", example = "Pikachu")
         String name,
+
+        @Schema(description = "Tipo do Pokémon", example = "Electric")
         String type,
+
+        @Schema(description = "Altura do Pokémon", example = "40")
         Integer height,
+
+        @Schema(description = "Peso do Pokémon", example = "60")
         Integer weight,
+
+        @Schema(description = "Descrição do Pokémon", example = "Mouse Pokémon")
         String description,
+
+        @Schema(description = "URL da imagem do Pokémon", example = "https://example.com/pikachu.png")
         String imageUrl
 ) {
 }
--- src/main/java/com/matheus/pokedexapi/application/dto/UpdatePokemonRequest.java
+++ src/main/java/com/matheus/pokedexapi/application/dto/UpdatePokemonRequest.java
@@ -1,21 +1,28 @@
 package com.matheus.pokedexapi.application.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record UpdatePokemonRequest(
 
+        @Schema(description = "Nome do Pokémon", example = "Pikachu")
         @NotBlank(message = "Name is required")
         String name,
 
+        @Schema(description = "Tipo do Pokémon", example = "Electric")
         @NotBlank(message = "Type is required")
         String type,
 
+        @Schema(description = "Altura do Pokémon", example = "40")
         Integer height,
 
+        @Schema(description = "Peso do Pokémon", example = "60")
         Integer weight,
 
+        @Schema(description = "Descrição do Pokémon", example = "Mouse Pokémon")
         String description,
 
+        @Schema(description = "URL da imagem do Pokémon", example = "https://example.com/pikachu.png")
         String imageUrl
 ) {
 }
